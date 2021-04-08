b0452f7e125ad969d2ac6e49bda981024d4e3499
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-04-08 16:54:04 +0300

    Add files via upload

---
+++ RegexFiles/SplitString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SplitString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = "1   5  3 2 111 888      5 6";
+            string[] splitted = Regex.Split(numbers, @"\s+");
+            Console.WriteLine(string.Join(',',splitted));
+        }
+    }
+}
