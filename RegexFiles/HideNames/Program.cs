8cef69a3b72387dace1dbb8878f166ec5d0f67d1
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-04-08 16:41:13 +0300

    Add files via upload

---
+++ RegexFiles/HideNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HideNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<firstName>[A-Z][a-z]+) (?<lastName>[A-Z][a-z]+)";
+            Regex reg = new Regex(pattern);
+            string input = Console.ReadLine();
+            var hidden = reg.Replace(input,"*** ***");
+            Console.WriteLine(hidden);
+        }
+    }
+}
