c310b4812c81863966538c0048db5ec65e8a78a5
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-04-15 23:58:37 +0300

    Add files via upload

---
+++ RegexFiles/SoftUniBarIncome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SoftUniBarIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^[^|$%.]*%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<quantity>\d+)\|[^|$%.]*?(?<price>(\d+\.?\d*))[^|$%.]*\$$");
+
+            double income = 0;
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if(line == "end of shift")
+                {
+                    break;
+                }
+
+                Match match = regex.Match(line);
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                string customer = match.Groups["customer"].Value;
+                string product = match.Groups["product"].Value;
+                int quantity = int.Parse(match.Groups["quantity"].Value);
+                double price = double.Parse(match.Groups["price"].Value);
+
+                double totalCustomerIncome = price * quantity;
+                Console.WriteLine($"{customer}: {product} - {totalCustomerIncome:F2}");
+                income += totalCustomerIncome;
+            }
+            Console.WriteLine($"Total income: {income:F2}");
+        }
+    }
+}
