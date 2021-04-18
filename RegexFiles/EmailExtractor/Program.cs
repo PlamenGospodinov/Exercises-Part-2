d2d1e51240a810af228ec85428ef8ea2efb3fbd9
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-04-18 22:23:14 +0300

    Add files via upload

---
+++ RegexFiles/EmailExtractor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(^|(?<=\s))[A-Za-z\d]+([-._][A-Za-z\d]+)*@[A-Za-z]+(\-[A-Za-z]+)*(\.[A-Za-z]+)+");
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(text);
+
+
+            Console.WriteLine("All emails are: ");
+            foreach(Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
