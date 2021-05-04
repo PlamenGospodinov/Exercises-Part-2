3101f17c71a92882f882cd03fd5def7e97f8d118
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-05-04 23:42:26 +0300

    Add files via upload

---
+++ MVC+SOAP/ApplicationService/DTOs/StudentDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationService.DTOs
+{
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string FacultyNumber { get; set; }
+        public int YearsInUniversity { get; set; }
+
+    }
+}
---
+++ MVC+SOAP/ApplicationService/Implementations/StudentManagementService.cs
@@ -0,0 +1,75 @@
+﻿using ApplicationService.DTOs;
+using Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+
+namespace ApplicationService.Implementations
+{
+    public class StudentManagementService
+    {
+        private Student1SystemDBContext ctx = new Student1SystemDBContext();
+
+        public List<StudentDTO> Get()
+        {
+            List<StudentDTO> studentsDto = new List<StudentDTO>();
+
+            foreach (var item in ctx.Students.ToList())
+            {
+                studentsDto.Add(new StudentDTO
+                {
+                    Id = item.Id,
+                    FirstName = item.FirstName,
+                    LastName = item.LastName,
+                    Age = item.Age,
+                    FacultyNumber = item.FacultyNumber,
+                    YearsInUniversity = item.YearsInUniversity
+                });
+            }
+
+            return studentsDto;
+        }
+
+        public bool Save(StudentDTO studentDTO)
+        {
+            Student Student = new Student
+            {
+                FirstName = studentDTO.FirstName,
+                LastName = studentDTO.LastName,
+                Age = studentDTO.Age,
+                FacultyNumber = studentDTO.FacultyNumber,
+                YearsInUniversity = studentDTO.YearsInUniversity
+            };
+
+            try
+            {
+                ctx.Students.Add(Student);
+                ctx.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                Student student = ctx.Students.Find(id);
+                ctx.Students.Remove(student);
+                ctx.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
