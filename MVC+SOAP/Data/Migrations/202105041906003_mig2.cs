bcbee9ba3982775dc1a8463b65ff37acc9231298
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-05-04 23:43:18 +0300

    Add files via upload

---
+++ MVC+SOAP/Data/Migrations/202105041906003_mig2.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Students", "Age", c => c.Int(nullable: false));
+            AddColumn("dbo.Students", "FacultyNumber", c => c.String());
+            AddColumn("dbo.Students", "YearsInUniversity", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Students", "YearsInUniversity");
+            DropColumn("dbo.Students", "FacultyNumber");
+            DropColumn("dbo.Students", "Age");
+        }
+    }
+}
