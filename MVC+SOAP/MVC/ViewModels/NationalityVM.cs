51b3d9f85e1b632c41335d52b9ee21f05c826349
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-05-04 23:46:45 +0300

    Add files via upload

---
+++ MVC+SOAP/MVC/ViewModels/NationalityVM.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationService.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.ViewModels
+{
+    public class NationalityVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public NationalityVM()
+        {
+
+        }
+
+        public NationalityVM(NationalityDTO nationalityDTO)
+        {
+            Id = nationalityDTO.Id;
+            Title = nationalityDTO.Title;
+        }
+
+    }
+}
