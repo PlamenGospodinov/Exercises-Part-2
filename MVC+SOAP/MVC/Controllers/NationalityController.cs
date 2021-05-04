64d87e15c8fd49949c26723dd4f3f2316d2b2777
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-05-04 23:47:37 +0300

    Add files via upload

---
+++ MVC+SOAP/MVC/Controllers/NationalityController.cs
@@ -0,0 +1,65 @@
+﻿using ApplicationService.DTOs;
+using MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class NationalityController : Controller
+    {
+        // GET: Nationality
+        public ActionResult Index()
+        {
+            List<NationalityVM> nationalitiesVM = new List<NationalityVM>();
+
+            using(SOAPService.Service1Client service = new SOAPService.Service1Client())
+            {
+                foreach(var item in service.GetNationalities())
+                {
+                    nationalitiesVM.Add(new NationalityVM(item));
+                }
+            }
+            return View(nationalitiesVM);
+        }
+
+        public ActionResult Create(NationalityVM nationalityVM)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    using (SOAPService.Service1Client service = new SOAPService.Service1Client())
+                    {
+                        NationalityDTO nationalityDTO = new NationalityDTO
+                        {
+                            Title = nationalityVM.Title
+                        };
+                        service.PostNationality(nationalityDTO);
+
+                        return RedirectToAction("Index");
+                    }
+                    
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+            
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using(SOAPService.Service1Client service = new SOAPService.Service1Client())
+            {
+                service.DeleteNationality(id);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
