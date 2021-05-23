70824ecb6473ac86930682eb9e2ea26094daa259
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-05-23 23:29:54 +0300

    Add files via upload

---
+++ Hackerrank/ExceptionsHackerrank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExceptionsHackerrank
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(S);
+                Console.WriteLine(number);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception detected!");
+            }
+        }
+    }
+}
