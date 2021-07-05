f00d385d56522d26d2a0a4c4a7df304fcc8e6e69
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-07-05 23:52:53 +0300

    Add files via upload

---
+++ Object-Oriented Programming/UsernamePasswordID/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace UsernamePasswordID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Username = Console.ReadLine();
+            user.Password = Console.ReadLine();
+            Console.WriteLine(user.ID);
+        }
+    }
+
+    class User
+    {
+        public static int currentID;
+        public readonly int ID;
+        private string username;
+        private string password;
+        public User()
+        {
+            currentID++;
+            ID = currentID;
+        }
+
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+            currentID++;
+            ID = currentID;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                if (value.Length <= 3 || value == "")
+                {
+                    Console.WriteLine("The username you set is not proper!");
+                }
+                else
+                {
+                    value = username;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (value.Length <= 3 || value == "")
+                {
+                    Console.WriteLine("The password you set is not proper!");
+                }
+                else
+                {
+                    value = password;
+                }
+            }
+        }
+    }
+}
