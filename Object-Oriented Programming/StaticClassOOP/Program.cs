41ee8808bcb45c03235d026d6347e9c219ae162b
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-07-06 23:22:54 +0300

    Add files via upload

---
+++ Object-Oriented Programming/StaticClassOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StaticClassOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities.ColorConsoleWrite("Hello guys!", ConsoleColor.DarkBlue);
+        }
+    }
+}
---
+++ Object-Oriented Programming/StaticClassOOP/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticClassOOP
+{
+    static class Utilities
+    {
+
+        public static void ColorConsoleWrite(string message,ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
