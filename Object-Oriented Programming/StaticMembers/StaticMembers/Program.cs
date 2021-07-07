91eb732d7ba17d019a0858bfea6c632c989e64ac
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-07-07 20:17:29 +0300

    Add files via upload

---
+++ Object-Oriented Programming/StaticMembers/StaticMembers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticMembers
+{
+
+    public class Sequence
+    {
+        private static int currentValue = 0;
+        private Sequence()
+        {
+
+        }
+
+        public static int NextValue()
+        {
+            currentValue++;
+            return currentValue;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sequence[1...3]: {0},{1},{2}",Sequence.NextValue(), Sequence.NextValue(), Sequence.NextValue());
+        }
+    }
+}
