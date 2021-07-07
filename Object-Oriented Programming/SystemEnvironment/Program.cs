4eab8b30ad1dff87f880d95cee9ae39800ae0de5
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-07-07 20:25:25 +0300

    Add files via upload

---
+++ Object-Oriented Programming/SystemEnvironment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SystemEnvironment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int startTime = Environment.TickCount;
+
+            for(int i = 0; i < 10000000; i++)
+            {
+                sum++;
+            }
+
+            int endTime = Environment.TickCount;
+            Console.WriteLine("The time elapsed is: {0} seconds.",(endTime-startTime)/1000.0);
+        }
+    }
+}
