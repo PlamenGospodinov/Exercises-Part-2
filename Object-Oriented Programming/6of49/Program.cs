5102e9bc69f3c981fbcbb4b6d099d84d2b00b4f7
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-07-07 20:38:02 +0300

    Add files via upload

---
+++ Object-Oriented Programming/6of49/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6of49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            for(int i = 0; i < 6; i++)
+            {
+                int randomNumber = rand.Next(49) + 1;
+                Console.Write("{0} ",randomNumber);
+            }
+        }
+    }
+}
