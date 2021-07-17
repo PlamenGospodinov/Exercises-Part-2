6840d08d734c6e0bd7aa6e3a198490bbbc0efc1f
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-07-17 14:17:04 +0300

    Add files via upload

---
+++ TCP-IP Socket Programming/SocketServerStarter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddr = IPAddress.Any;
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+            listenerSocket.Bind(ipep);
+            listenerSocket.Listen(5);
+            listenerSocket.Accept();
+        }
+    }
+}
