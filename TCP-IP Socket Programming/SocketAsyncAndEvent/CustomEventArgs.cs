dfdf8d3d51780353cee758400773514df775111f
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-08-07 23:39:08 +0300

    Add files via upload

---
+++ TCP-IP Socket Programming/SocketAsyncAndEvent/CustomEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketAsync2
+{
+    public class ClientConnectedEventArgs : EventArgs
+    {
+        public string NewClient { get; set; }
+
+        public ClientConnectedEventArgs(string _newClient)
+        {
+            NewClient = _newClient;
+        }
+    }
+    public class TextReceivedEventArgs : EventArgs
+    {
+        public string ClientWhoSentText { get; set; }
+
+        public string TextReceived { get; set; }
+
+        public TextReceivedEventArgs(string _clientWhoSentText,string _textReceived)
+        {
+            ClientWhoSentText = _clientWhoSentText;
+            TextReceived = _textReceived;
+        }
+    }
+
+}
---
+++ TCP-IP Socket Programming/SocketAsyncAndEvent/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SocketAsync2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SocketAsync2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c69a2e45-b4a6-4f69-a778-62ecb50c0e20")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
