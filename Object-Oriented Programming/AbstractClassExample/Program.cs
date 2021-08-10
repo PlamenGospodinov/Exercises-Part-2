94002a4769041def7960c5c02ce6e1149f4f60bf
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-08-11 00:19:32 +0300

    Add files via upload

---
+++ Object-Oriented Programming/AbstractClassExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExample
+{
+    public abstract class Animal
+    {
+        public void PrintInformation()
+        {
+            Console.WriteLine("I am {0}.",this.GetType().Name);
+            Console.WriteLine(GetTypicalSound());
+        }
+
+        protected abstract String GetTypicalSound();
+    }
+
+    public class Cat : Animal
+    {
+        protected override string GetTypicalSound()
+        {
+            return "Meowwww";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat();
+            cat.PrintInformation();
+            Console.ReadLine();
+        }
+    }
+}
