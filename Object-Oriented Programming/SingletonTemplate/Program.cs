0a6c1d8361cc7ee82de8ee59f5ff758cfea55cc2
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-08-23 14:05:22 +0300

    Add files via upload

---
+++ Object-Oriented Programming/SingletonTemplate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Singleton
+    {
+        //Single instance
+        private static Singleton instance;
+
+        //Initialize the single instance
+        static Singleton()
+        {
+            instance = new Singleton();
+        }
+
+        //The property for taking the single instance
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        //Private constructor - protests direct instantiation
+        private Singleton()
+        {
+
+        }
+    }
+}
