dddc32bf422f4d6b4a2b0a4352f311f8ce2284d5
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-08-29 17:46:36 +0300

    Add files via upload

---
+++ Object-Oriented Programming/InterfaceExample/IEmail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    interface IEmail
+    {
+        void SendEmail();
+    }
+
+    class OutlookEmail : IEmail
+    {
+        public void SendEmail()
+        {
+            Console.WriteLine("send outlook email");
+        }
+    }
+
+    class WebserviceEmail : IEmail
+    {
+        public void SendEmail()
+        {
+            Console.WriteLine("send webservice email");
+        }
+    }
+}
---
+++ Object-Oriented Programming/InterfaceExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    class Employee
+    {
+        private int empId;
+        private string empName;
+        public double salary { get; set; }
+        public int grade { get; set; }
+        private string company = "IBM";
+
+        IEmail email;
+
+        public Employee(int empId,string empName,IEmail email)
+        {
+            this.empId = empId;
+            this.empName = empName;
+            this.email = email;
+        }
+
+        public void NotifyEmployee()
+        {
+            email.SendEmail();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmail email = new OutlookEmail();
+            Employee emp1 = new Employee(100, "Alex",email);
+            emp1.salary = 100000;
+            emp1.NotifyEmployee();
+
+            email = new WebserviceEmail();
+            Employee emp2 = new Employee(100, "Emily",email);
+            emp2.salary = 20000;
+        }
+    }
+}
