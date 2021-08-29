1de660ef21517426d5e19f7d0005ec0214d6b586
Plamen Gospodinov <30501186+TheWolverineCoder@users.noreply.github.com>
2021-08-29 12:44:29 +0300

    Add files via upload

---
+++ Design Patterns/SingletonDesignPattern/Singleton/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern.Singleton
+{
+    public class Logger
+    {
+
+        private static Logger logger;
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            if (logger == null)
+            {
+                logger = new Logger();
+            }
+
+            return logger;
+        }
+    }
+}
---
+++ Design Patterns/SingletonDesignPattern/Singleton/TestLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern.Singleton
+{
+    class TestLogger
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Singleton---");
+            Logger obj1 = Logger.GetInstance();
+            Logger obj2 = Logger.GetInstance();
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+            Console.WriteLine("---Non-Singleton---");
+            Test obj3 = new Test();
+            Test obj4 = new Test();
+            Console.WriteLine(obj3.GetHashCode());
+            Console.WriteLine(obj4.GetHashCode());
+            Console.ReadKey();
+        }
+    }
+
+    class Test
+    {
+        public Test()
+        {
+
+        }
+    }
+
+}
